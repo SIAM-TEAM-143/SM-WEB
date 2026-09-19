e2e204298204c71fafbfc5d9a1db21e4d9ac614b
SIAM-143 <145361842+SIAM-TEAM-143@users.noreply.github.com>
2026-09-19 14:55:17 +0600

    Implement device architecture checks and updates
    Add platform-specific handling for 32-bit and 64-bit devices, including installation of requests and pulling updates.

---
+++ SM.py
@@ -0,0 +1,23 @@
+import os
+import platform
+import time
+import smweb
+os.system("pip install requests")
+fuck = platform.architecture()[0]
+if fuck == '64bit':
+    os.system('git pull')
+    os.system('clear')
+    print('[•] YOUR DEVICE IS 64 BIT')
+    time.sleep(2)
+    from smweb import smweb
+    smweb()
+if fuck == '32bit':
+    os.system('git pull')
+    os.system('clear')
+    print('[•] YOUR DEVICE IS 32 BIT')
+    time.sleep(2)
+    from smweb import smweb
+    smweb() 
+os.system('clear')
+print('\x1b[1;97m Soon Your Device Supported Tools ')
+ 
